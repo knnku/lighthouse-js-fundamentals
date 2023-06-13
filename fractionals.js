b47bd8f8a11038d43871389922246657c2cd40e9
Kenniku Tolato <kenniku_you@ymail.com>
2023-06-13 04:32:06 +0000

    Added loopy and modified fractional running files

--- fractionals.js
+++ fractionals.js
@@ -1,22 +1,11 @@
 
-let num = 10;
-let solution = 0;
+num = 9;
+solution = ''
 
 for (var i = num - 1; i >= 1; i--) {
-    // We store the value of num at each iteration
-    solution *= i; // or num *= i;
-    console.log(i);
-    /* 
-                    num      var i = num - 1       num *= i         i--       i >= 1?
-    1st iteration:   5           4 = 5 - 1         20 = 5 * 4        3          yes   
-    2nd iteration:  20           3 = 4 - 1         60 = 20 * 3       2          yes
-    3rd iteration:  60           2 = 3 - 1        120 = 60 * 2       1          yes  
-    4th iteration: 120           1 = 2 - 1        120 = 120 * 1      0          no             
-    5th iteration: 120               0                120
-    End of the FOR loop 
-    */
+    solution = num *= i;
+    
 }
-
 console.log(solution);
 
 
