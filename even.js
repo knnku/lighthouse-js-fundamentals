514db69d36d0474000d2b6e39bfbe571224aacb6
Kenniku Tolato <kenniku_you@ymail.com>
2023-06-15 05:51:52 +0000

    Addded even.js file from prep direct module.

---
+++ even.js
@@ -0,0 +1,16 @@
+const isEven  = function(num) {
+  return num % 2 === 0;
+
+ 
+}
+
+console.log(isEven(6));
+
+//We can simplify our above code a little by learning that we don't need to set our function return values to new variables before doing something with them–we can use them directly where we need them.
+
+
+console.log(isEven(10));
+console.log(isEven(11));
+
+
+
--- hello.js
+++ hello.js
@@ -1,5 +1,13 @@
+/*
 const sayHello  = function (name) {
   console.log("Hello, world! " + name);
 }
 
 sayHello("Ken");
+*/
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
