7e36bcd0cb6d977dbb9d189ece801597e5293e99
Kenniku Tolato <kenniku_you@ymail.com>
2023-06-18 19:07:46 +0000

    Add array loop additionals.

---
+++ packing-list.js
@@ -0,0 +1,16 @@
+//For loop to iterate arrays
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
+
+
+//While loop to iterate arrays
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
