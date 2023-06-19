0d2f9ab6f84b51d159942d1de2486389e9171660
Kenniku Tolato <kenniku_you@ymail.com>
2023-06-19 00:08:55 +0000

    Add ingredients .js file for array loops challenge.

---
+++ ingredients.js
@@ -0,0 +1,27 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+
+// Write a for loop that prints out the contents of ingredients:
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+
+var i = "0"
+
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+
+  i++
+}
+
+for (i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+
+
+for (x = ingredients.length; x > -1; x--) {
+  console.log(ingredients[x]);
+}
+
+
+
