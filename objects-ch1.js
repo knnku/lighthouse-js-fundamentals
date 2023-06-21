21073ea3f58f344a821255f62e5b173f30c7372b
Kenniku Tolato <kenniku_you@ymail.com>
2023-06-21 17:41:09 +0000

    Add files for object challenges.

---
+++ objects-ch1.js
@@ -0,0 +1,17 @@
+/*
+ * Programming Quiz: Menu Items (7-2)
+ * Create an object named `breakfast`. 
+ * The object should contain properties for the `name`, `price`, and `ingredients`.
+ * Print the entire object on the console
+ */
+
+// your code goes here
+
+
+var breakfast = {
+  name: "English Breakfast",
+  price: 10.99,
+  ingredients: ["Eggs", "Bacon", "Bread"]
+}
+
+console.log(breakfast.ingredients[1])
--- parade.js
+++ parade.js
@@ -1,13 +1,12 @@
 
-const moves =  
+const moves = ['north', 'north', 'south', 'north']  
 
 function finalPosition(moves) {
   let yPos = 0;
   let xPos = 0;
 
-  let pos = [];
-
-  for (let moves of moves) {
+  let pos = 0;
+  for (let move of moves) {
     if (move === "north") {
       yPos + 1;
     } else if (move === "south") {
@@ -16,13 +15,16 @@ function finalPosition(moves) {
       xPos + 1;
     } else if (move === "west") {
       xPos - 1;
+    } else {
+      xPos + 0 && yPos + 0;
     }
 
     
   }
 
-  return xPos, yPos;
+  pos.push()
+  return pos;
 
 }
 
-console.log(finalPosition(['north', 'north', 'south', 'north'] ))
+console.log(finalPosition(moves))
