bd3111f407e364f45b705b125d6331990437d069
Kenniku Tolato <kenniku_you@ymail.com>
2023-06-22 01:25:27 +0000

    Add object-donuts file.

---
+++ objects-donuts.js
@@ -0,0 +1,28 @@
+/*
+ * Programming Quiz: Donuts Revisited (7-6)
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code sshould have an array named `donuts`
+ * - Your `donuts` array should call the `forEach()` method
+ * - Your `forEach()` loop should output the donut summaries
+ * - BE CAREFUL ABOUT THE PUNCTUATION, SPACES, AND EXACT WORDS TO BE PRINTED.
+ */
+
+// This is an array of objects. 
+var donuts = [
+  { type: "Jelly", cost: 1.22 },
+  { type: "Chocolate", cost: 2.45 },
+  { type: "Cider", cost: 1.59 },
+  { type: "Boston Cream", cost: 5.99 }
+];
+
+
+
+// your code goes here
+//Boston Cream donuts cost $5.99 each
+
+
+
+donuts.forEach(item => console.log(item.type+ " donuts cost $" + item.cost + " each"))
